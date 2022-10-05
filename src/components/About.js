acda6536b3442fee5ac2ce36b917bb1563e17ad0
Mark Muyuela <mark.muyuela81@gmail.com>
2022-10-05 17:53:11 +0100

    interests images added

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
 import instagram from '../styles/footerLogos/instagram.png'
 
-import placeholder from '../styles/about/jk-placeholder-image.jpeg'
 import coffee from '../styles/about/coffee.jpg'
 import print from '../styles/about/print.jpg'
-
+import tabletop from '../styles/about/tabletop.JPG'
+import bikepacked from '../styles/about/bikepacked.jpeg'
 
 const About = () => {
 
@@ -35,22 +35,22 @@ const About = () => {
             <p>But first, coffee!</p>
           </div>
         </div>
-        <h1>Interests</h1>
+        <h1 className="interest-header">Interests</h1>
         <div className="interests">
           
           <div className="print">
             <div id="print"></div>
-            <h1>3d Print</h1>
+            <h1 className="interest-header">3D Print</h1>
             <img className="print-image" src={print}></img>
             <div className="about-text">
               <p>I&#39;ve owned a couple of 3d printers over the past 5 years, the most recent one being my Ender 3 S1 as it was a lot quieter and printed in higher definition than the Cel Robox RB01 that it replaced.</p>
-              <p>I use it mostly to print practical items to help with tabletop gaming, or for simple solutions for everyday objects.</p>
+              <p>I use it mostly to print practical items to help with tabletop gaming, or simple solutions for everyday objects.</p>
             </div>
           </div>
           <div className="tabletop">
             <div id="tabletop"></div>
-            <h1>Tabletop Gaming</h1>
-            <img className="tabletop-image" src={placeholder}></img>
+            <h1 className="interest-header">Tabletop Gaming</h1>
+            <img className="tabletop-image" src={tabletop}></img>
             <div className="about-text">
               <p>Tabletop gaming is a great way of socialising, working on strategy and working through practical problems.  It&#39;s a great way of letting off steam and being creative.</p>
               <p>The gaming system I currently play is Blood Bowl, both for fun and at tournament level.  In the past I have also played Warhammer 40k, Infinity and X-wing the miniatures game.</p>
@@ -59,8 +59,8 @@ const About = () => {
           </div>
           <div className="bikepacking">
             <div id="bikepacking"></div>
-            <h1>Bikepacking</h1>
-            <img className="bikepacking-image" src={placeholder}></img>
+            <h1 className="interest-header">Bikepacking</h1>
+            <img className="bikepacking-image" src={bikepacked}></img>
             <div className="about-text">
               <p>Cycling has always been one my favourite pastimes.  As a youngster it symbolised freedom to travel, and as an adult that hasn&#39;t changed much!</p>
               <p>I started by getting into mountain biking, then progressing onto road and gravel.  Whilst still dabbling with those disciplines, bikepacking has become a real passion.  The ability to ride out into the wilderness, fully self-sufficient, and being able to cover distance gives the same sense of freedom I had as a kid.  Not forgetting the great views to wake up to if you can find a great spot to camp in!</p>
